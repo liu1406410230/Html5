00079d526a79f628e75df372521f47c86b5f1877
liujilin1406410230 <32945796+liujilin1406410230@users.noreply.github.com>
2017-11-16 16:51:55 +0800

    Delete app.js

--- js/app.js
+++
@@ -1,9 +0,0 @@
-var fs =
-    import ("fs");
-
-function callback(err, data) {
-    if (!err) {
-        console.log(data.split('\n').length - 1);
-    }
-}
-fs.readFile(process.argv[2], 'utf8', callback);
